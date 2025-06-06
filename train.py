79a78ed6163980f50be4e9734791c93cb5d307bb
Kusum Menaria <104812753+Menaria123@users.noreply.github.com>
2025-06-06 14:01:46 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,10 +1,18 @@
 import os
 
-# Get current working directory
+# Get absolute path to Images folder
 cwd = os.getcwd()
-
-# Join to get the full path to the Images folder
 images_path = os.path.join(cwd, "Images")
 
-# Print the full path
-print("Absolute path to 'Images' folder:", images_path)
+# Write full paths to data.yaml
+yaml_content = f"""
+train: {images_path}/train
+val: {images_path}/val
+nc: 2
+names: ['door', 'window']
+"""
+
+with open("data.yaml", "w") as f:
+    f.write(yaml_content.strip())
+
+print("data.yaml file created with full image paths.")
