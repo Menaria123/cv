8eb3c2701a4d446b0e340eb8a0974b86589312bc
Kusum Menaria <104812753+Menaria123@users.noreply.github.com>
2025-06-06 21:26:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -81,5 +81,6 @@ def detect():
     return jsonify({"detections": detections})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))  # fallback to 10000
     app.run(host="0.0.0.0", port=port, debug=True)
+
