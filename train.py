92317f0d95b24f24333e28f1ce9c013fb963380c
Kusum Menaria <104812753+Menaria123@users.noreply.github.com>
2025-06-06 12:43:41 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,18 +1,2 @@
-import torch
-import ultralytics.nn.modules
-import ultralytics.nn.tasks
-from ultralytics import YOLO
-import os 
-print("CWD:", os.getcwd())
-
-model = YOLO('yolov8n.pt')
-
-# Train on your custom door-window dataset
-model.train(
-    data='data.yaml',  
-    epochs=100,
-    imgsz=640,
-    batch=16,
-    name='door_window_yolov8',
-    device='cpu'  
-)
+import os
+print("Current Working Directory:", os.getcwd())
