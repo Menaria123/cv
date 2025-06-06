09a55e962a323f74d1693abfbed06f7a58e97274
Kusum Menaria <104812753+Menaria123@users.noreply.github.com>
2025-06-06 13:08:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,19 @@ import os
 import torch
 import ultralytics.nn.modules
 import ultralytics.nn.tasks
+import torch
+# Load base model (YOLOv8n pretrained weights)
+model = YOLO('yolov8n.pt')
 
+# Train on your custom door-window dataset
+model.train(
+    data=r"data.yaml",  
+    epochs=100,
+    imgsz=640,
+    batch=16,
+    name='door_window_yolov8',
+    device='cpu'  
+)
 app = Flask(__name__)
 
 
