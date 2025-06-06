f80ca43985f4597909f99602099b3010dfd24417
Kusum Menaria <104812753+Menaria123@users.noreply.github.com>
2025-06-06 13:25:16 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,18 +1,6 @@
-import torch
-import ultralytics.nn.modules
-import ultralytics.nn.tasks
-from ultralytics import YOLO
+import os
 
-
-# Load base model (YOLOv8n pretrained weights)
-model = YOLO('yolov8n.pt')
-
-# Train on your custom door-window dataset
-model.train(
-    data=r"data.yaml",  
-    epochs=100,
-    imgsz=640,
-    batch=16,
-    name='door_window_yolov8',
-    device='cpu'  
-)
+print("PWD:", os.getcwd())
+print("Files in current dir:", os.listdir('.'))
+print("Files in images/val:", os.listdir('./images/val') if os.path.exists('./images/val') else "No val folder")
+print("Files in images/train:", os.listdir('./images/train') if os.path.exists('./images/train') else "No train folder")
