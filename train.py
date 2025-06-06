20c5c37c2d96ae572ce9a4d227c3da89f85ad61a
Kusum Menaria <104812753+Menaria123@users.noreply.github.com>
2025-06-06 09:37:45 +0530

    Update train.py

--- train.py
+++ train.py
@@ -2,14 +2,16 @@ import torch
 import ultralytics.nn.modules
 import ultralytics.nn.tasks
 from ultralytics import YOLO
+import os 
 
-
+base_dir = os.path.dirname(__file__)  # Path where this script is located
+data_yaml = os.path.join(base_dir, "data/data.yaml")
 # Load base model (YOLOv8n pretrained weights)
 model = YOLO('yolov8n.pt')
 
 # Train on your custom door-window dataset
 model.train(
-    data=r"\data.yaml",  
+    data=data_yaml,  
     epochs=100,
     imgsz=640,
     batch=16,
